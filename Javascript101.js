440e5b2c1aefd83ce433748daff840ca491ea543
plaetzaw <plaetzaw@gmail.com>
2020-02-04 15:36:24 -0600

    Nearly Done

--- Javascript101.js
+++ Javascript101.js
@@ -66,32 +66,98 @@
 
 // `If/Else Blocks`
 
-var expr = "Papayas";
-if (expr == "Oranges") {
-  console.log("Oranges are $0.59 a pound.");
-} else if (expr == "Mangoes") {
-  console.log("Mangoes are $3.89 a poud OMG.");
-} else if (expr == "Papayas") {
-  console.log("Mangoes and papayas are $2.79 a pound.");
-} else {
-  console.log("Sorry, we are out of " + expr + ".");
-}
+// var expr = "Papayas";
+// if (expr == "Oranges") {
+//   console.log("Oranges are $0.59 a pound.");
+// } else if (expr == "Mangoes") {
+//   console.log("Mangoes are $3.89 a poud OMG.");
+// } else if (expr == "Papayas") {
+//   console.log("Mangoes and papayas are $2.79 a pound.");
+// } else {
+//   console.log("Sorry, we are out of " + expr + ".");
+// }
 
-var expr = "Papayas";
-switch (expr) {
-  case "Oranges":
-    console.log("Oranges are $0.59 a pound.");
-    break;
+// var expr = "Papayas";
+// switch (expr) {
+//   case "Oranges":
+//     console.log("Oranges are $0.59 a pound.");
+//     break;
 
-  case "Mangoes":
-    console.log("Oranges are $0.59 a pound.");
-    break;
+//   case "Mangoes":
+//     console.log("Oranges are $0.59 a pound.");
+//     break;
 
-  case "Papayas":
-    console.log("Mangoes and papayas are $2.79 a pound.");
-    break;
+//   case "Papayas":
+//     console.log("Mangoes and papayas are $2.79 a pound.");
+//     break;
 
-  default:
-    console.log("Sorry, we are out of " + expr + ".");
-    break;
-}
+//   default:
+//     console.log("Sorry, we are out of " + expr + ".");
+//     break;
+// }
+
+// //While Loops
+// var count = 0;
+
+// while (count < 10) {
+//   count++; //count = count + 1
+//   console.log(count);
+// }
+
+//For Loops
+// for (var count = 0; count < 10; count++) {
+//   console.log(count);
+//   for (var inner = 0; inner < 3; inner++) {
+//     console.log(inner);
+//   }
+// }
+
+//Collections
+// var lottoNums = [23, 11, 43, 19, 37, 16];
+// console.log(lottoNums);
+// console.log(lottoNums[0]);
+// console.log(lottoNums[3]);
+
+//Array.pop()
+// var Popped = lottoNums.pop();
+// console.log(Popped);
+
+//Array.shift()
+// var Shifted = lottoNums.shift();
+// console.log(Shifted);
+
+//Array.splice(beginl, num)
+// var Spliced = lottoNums.splice(2, 1);
+// var Spliced = lottoNums.splice(2, 3);
+// console.log(lottoNums);
+// console.log(Spliced);
+
+//Array.slice(beginl, endl)
+// var Sliced = lottoNums.slice(2, 3);
+// console.log(Sliced);
+
+//S.split('seperator')
+// var words = "oh hey how is it going".split("");
+// console.log(words);
+
+// S.split with for loop
+// var words = "oh hey how is it going".split(" ");
+// for (var i = 0; i < words.length; i++) {
+//   console.log(words[i]);
+// }
+
+//Array.join()
+// var words = "oh hey how is it going".split(" ");
+// for (var i = 0; i < words.length; i++) {
+//   console.log(words[i]);
+// }
+// console.log(words.join(" "));
+
+// JS Objects
+var characterSheet = {
+  name: "tim berners-lee",
+  title: "sir",
+  powers: "invent the web"
+};
+
+console.log("name");
