c631f4f5514944c1552f424d2a3714ecbecba3ea
plaetzaw <plaetzaw@gmail.com>
2020-02-04 13:13:14 -0600

    Working on HW

---
+++ Javascript101.js
@@ -0,0 +1,97 @@
+// console.log('Hello You SMelly Animals');
+
+// var school = "DigitalCrafts"
+
+// function myFunction(){
+
+// }
+
+// console.log('Today is a good day to wake up and then go back to sleep')
+
+// #Backtick
+// `Wlkasjflkjsad
+// alskdjflkasdjf
+// slkfjklasjflk
+
+"hello" + " " + "there";
+
+// var myString = "hello";
+
+// var myInt = 2.1;
+
+// var myBool = true;
+
+// no len function
+
+// var hey = "Hello There";
+// console.log(hey.length)
+
+// var num = 1; //intiger
+
+// var num2 = 1.2; //float
+
+// var myString = "school";
+
+// var concat = "hello" + "world"
+// console.log(concat.indexOf("world"));
+
+// `Boolenas`
+
+// var boolean = true;
+// var not = !boolean; //false
+// var and = boolean && false; //false
+// var or = boolean || false; // true;
+// var equals = true == false; //false;
+
+// `If Statements`
+
+// var a = 4;
+// var b = 4;
+
+// if (a == b){
+// console.log("equality")
+
+// }
+
+// `If-else Statements`
+
+// var age = 22;
+// if (age >= 21) {
+//     console.log("You get free beer");
+//   } else if (age < 18) {
+//     console.log("What are you even doing here?");
+//   } else {
+//     console.log("Sorry no beer for you");
+//   }
+
+// `If/Else Blocks`
+
+var expr = "Papayas";
+if (expr == "Oranges") {
+  console.log("Oranges are $0.59 a pound.");
+} else if (expr == "Mangoes") {
+  console.log("Mangoes are $3.89 a poud OMG.");
+} else if (expr == "Papayas") {
+  console.log("Mangoes and papayas are $2.79 a pound.");
+} else {
+  console.log("Sorry, we are out of " + expr + ".");
+}
+
+var expr = "Papayas";
+switch (expr) {
+  case "Oranges":
+    console.log("Oranges are $0.59 a pound.");
+    break;
+
+  case "Mangoes":
+    console.log("Oranges are $0.59 a pound.");
+    break;
+
+  case "Papayas":
+    console.log("Mangoes and papayas are $2.79 a pound.");
+    break;
+
+  default:
+    console.log("Sorry, we are out of " + expr + ".");
+    break;
+}
