38d2dbb9008a13047d804e98a8a097391fc25302
plaetzaw <plaetzaw@gmail.com>
2020-11-14 13:21:57 -0600

    JS Practice

---
+++ test.js
@@ -0,0 +1,55 @@
+// Write a function that accepts two arguements, an array of integers and a single integer, \
+//and checks to see if the single value is in the array once and only once. Returns a boolean.
+
+function filterArray (array, target){
+    let repeat = array.filter(item => item === target)
+    if (repeat.length === 1){
+        return true
+    } else {
+        return false
+    }
+}
+
+//testing
+console.log(filterArray([1,2,3,4,5,6,6,7], 7));
+console.log(filterArray([1,2,3,4,5,6,6,7], 6));
+console.log(filterArray([1,2,3,4,5,6,6,7], 0));
+
+// Given the list of students below, filter and display the students that have a GPA above 3
+
+let students = [
+    {
+      name: "Jessica",
+      grade: 3.5,
+    },
+    {
+      name: "Adam",
+      grade: 2.4,
+    },
+    {
+      name: "Evan",
+      grade: 4,
+    },
+    {
+      name: "George",
+      grade: 3.2,
+    },
+    {
+      name: "Heather",
+      grade: 1.5,
+    },
+    {
+      name: "Jessica",
+      grade: 3.5,
+    },
+    {
+      name: "Mark",
+      grade: 3.6,
+    },
+  ];
+
+function studentGrades(array, target){
+    let notFailures = array.filter(kid => kid.grade > target)
+    return notFailures
+}
+console.log(studentGrades(students, 3));
