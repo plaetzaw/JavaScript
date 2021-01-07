70312beb32bd01ca2ef5a8597fdb1fd8bc788035
plaetzaw <plaetzaw@gmail.com>
2021-01-07 15:39:01 -0600

    .this review

--- review.js
+++ review.js
@@ -5,21 +5,21 @@ function sum(){
     return a+b
 }
 //ES6
-let sum = (a,b) => a + b
+let sum2 = (a,b) => a + b
 
 //Old
 function isPositive(num){
     return num >= 0
 }
 //ES6 
-let isPositive = num => num >= 0
+let isPositive2 = num => num >= 0
 
 //Old
 function randomNum(){
     return Math.random
 }
 //ES6
-let randomNum = () => Math.random
+let randomNum2 = () => Math.random
 
 //Old
 document.addEventListener('click', function() {
@@ -27,3 +27,29 @@ document.addEventListener('click', function() {
 })
 //ES6
 document.addEventListener('click', () =>  console.log('click'))
+
+
+// .this review
+class Person{
+    constructor(name){
+        this.name = name
+    }
+
+    printNameArrow(){
+        //Keeps the scoping for this
+        setTimeout(() => {
+            console.log('Arrow ' + this.name)
+        }, 100)
+    }
+
+    printNameFunction(){
+        //Does not since its an anonymous function
+        setTimeout(function() {
+            console.log('Function ' + this.name)
+        }, 100)
+    }
+}
+
+let Ragnarok = new Person('Ragnarok')
+Ragnarok.printNameArrow()
+Ragnarok.printNameFunction()
